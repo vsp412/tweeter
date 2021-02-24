87ea59e53d72969523d27011aacf5154672e0ddc
Vineet Pande <vsp4991@gmail.com>
2021-02-24 17:57:47 +0000

    created the character count functionality

--- public/scripts/new_tweet.js
+++ public/scripts/new_tweet.js
@@ -1,12 +1,22 @@
-// const floatField = document.getElementById("tweet-text");
-// const floatContainer = document.getElementsByTagName("form")[0];
-// floatField.addEventListener('focus', () => {
-//   floatContainer.classList.add('active');
-// });
-// floatField.addEventListener('blur', () => {
-//   floatContainer.classList.remove('active');
-// });
 
-// const ip = document.getElementById("tweet-text");
-// console.log(ip)
 
+$(document).ready(function() {
+ 
+  
+  
+
+    $("#tweet-text").on('keyup', () => {
+
+    
+      let maxLen = $("#tweet-text").attr("maxLength");
+      let charCount = $("#tweet-text").val().length;
+      let displayCount = maxLen - charCount;
+      $("output").text(displayCount);
+
+    });
+
+  
+      
+  
+
+});
