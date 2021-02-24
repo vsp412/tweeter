b75562e790a6c1db6118d7bf45e9be826086ac16
Vineet Pande <vsp4991@gmail.com>
2021-02-24 17:18:50 +0000

    created the tweet box layout in css

---
+++ public/scripts/new_tweet.js
@@ -0,0 +1,12 @@
+// const floatField = document.getElementById("tweet-text");
+// const floatContainer = document.getElementsByTagName("form")[0];
+// floatField.addEventListener('focus', () => {
+//   floatContainer.classList.add('active');
+// });
+// floatField.addEventListener('blur', () => {
+//   floatContainer.classList.remove('active');
+// });
+
+// const ip = document.getElementById("tweet-text");
+// console.log(ip)
+
