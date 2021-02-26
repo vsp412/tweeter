1b42bb9a3ce0eabdfe09f63cc0e081846bb802b4
Vineet Pande <vsp4991@gmail.com>
2021-02-26 15:33:01 +0000

    counter character gets turned red after reaching 140

--- public/scripts/new_tweet.js
+++ public/scripts/new_tweet.js
@@ -5,10 +5,17 @@ $(document).ready(function() {
     $("#tweet-text").on('keyup', () => {
 
     
-      let maxLen = $("#tweet-text").attr("maxLength");
+      const maxLen = 140;
       let charCount = $("#tweet-text").val().trim().length;
       let displayCount = maxLen - charCount;
       $("output").text(displayCount);
+      if (displayCount < 0) {
+        console.log(displayCount);
+        $("output").css({'color':'red'})
+      } else {
+        $("output").css({'color':'#545149'})
+      }
+       
 
     });
 
